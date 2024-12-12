1ee11b5c6e16199488275094324005301ec55899
cyq3721 <cyq3721@hotmail.com>
2024-12-12 08:29:49 +0800

    site

--- Bob/processorder.php
+++ Bob/processorder.php
@@ -1,88 +1,76 @@
-<?php
-$tireqty = $_POST['tireqty'];
-$oilqty = $_POST['oilqty'];
-$sparkqty = $_POST['sparkqty'];
-
-$tireprice = 24.0;
-$oilprice = 180.0;
-$sparkprice = 30.0;
-
-define('TIREPRICE',24);
-define('OILPRICE',180);
-define('SPARKPRICE',30);
-define('RATE',0.15);
-
-$total = $tireqty*TIREPRICE + $oilqty*OILPRICE + $sparkqty*SPARKPRICE;
-$total_rate = $total*(1+RATE);
-echo(gettype($tireprice));
-settype($tireprice,'float');
-echo (gettype($tireprice));
-
-?>
 <!doctype html>
 <html>
-    <head>
-        <title>Bob's Auto Parts -- Order Results</title>
-        <meta charset="utf-8" />
-    </head>
-    <body>
-        <div style="text-align:center;">
-            <h1>Bob's Auto Parts</h1>
-            <h3>Order Results</h3>
-            <hr>
-            <p>
-                Order processed at:
-            <?php echo date('H:i, JS F Y'); ?>
-            </p>
-            <table style="text-align:center;border:0px;">
-                <tr style="background:#cccccc;">
-                    <td style="width:150px;">Item</td>
-                    <td style="width:20px;">number</td>
-                    <td style="width:20px;">price</td>
-                </tr>
-                <tr>
-                    <td>Tires</td>
-                    <td>
-                        <?php echo $tireqty; ?>
-                    </td>
-                    <td>
-                        <?php echo "￥".($tireprice)*($tireqty); ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Oill</td>
-                    <td>
-                        <?php echo $oilqty; ?>
-                    </td>
-                    <td>
-                        <?php echo "￥".($oilprice)*($oilqty); ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td >SparkPlugs</td>
-                    <td>
-                        <?php echo $sparkqty; ?>
-                    </td>
-                    <td>
-                        <?php echo "￥".($sparkprice)*($sparkqty); ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="3" style="background:#bbccaa; text-align:center;" >
-                        <?php echo "总计：". $total; ?>
-                    </td>
-                </tr>
-                <tr>
-                  <td colspan="3" style="text-align:center;background:#aabbcc;">
-<?php echo "税后总计：" . number_format($total_rate,2); ?>
-                  </td>
-                </tr>
-            </table>
-            <?php
-     if($total == 0){
-         echo '<p style="background:green;">你没有购买任何商品 </p>';
-            }
-                        ?>
-        </div>
-    </body>
+  <head>
+    <title>Bob's Auto Parts - Order Results</title>
+    <meta charset="utf-8" />
+    <link href="./css/Bob.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <h1>Bob's Auto Parts</h1>
+    <h2>Order Results</h2>
+    <p>Order processed at:
+      <?php
+      echo date('H:i, js F Y');
+      
+      $tireqty =htmlspecialchars($_POST['tireqty']);
+      $oilqty = htmlspecialchars($_POST['oilqty']);
+      $sparkqty = htmlspecialchars($_POST['sparkqty']);
+
+      $total = $tireqty + $oilqty + $sparkqty;
+      ?>
+    </p>
+    <form id="form" method="POST"></form>
+    <table>
+      <tr>
+        <th class="caption1">商品</th>
+        <th class="caption2">数量</th>
+        <th class="caption2">售价</th>
+      </tr>
+      <tr>
+        <td >Oil</td>
+        <td class="value">
+          <?php echo $oilqty; ?>
+        </td>
+        <td>
+          <input name="oil_price" type="text" placeholder="请输入Oil的单价" for="form">
+        </td>
+      </Tr>
+       <tr>
+        <td>Tire</td>
+        <td class="value">
+          <?php echo $tireqty; ?>
+        </td>
+        <td>
+          <input name="tire_price" type="text" placeholder="请输入Tire的单价" for="form">
+        </td>
+       </tr>
+        <tr>
+        <td>Spark Plug</td>
+        <td class="value">
+          <?php echo $sparkqty; ?>
+        </td>
+        <td>
+          <input name="spark_price" type="text" placeholder="请输入Spark的单价" for="form">
+        </td>
+        </tr>
+        <tr>
+          <th class="caption1">总数</th>
+          <th class="caption2">
+            <?php echo $total; ?>
+          </th>
+          <?php
+          $oil_price = $_POST['oil_price'];
+          $tire_price = $_POST['tire_price'];
+          $spark_price = $_POST['spark_price'];
+          $total_price = $oil_price + $tire_price + $spark_price;
+          ?>
+          <th class="caption2">
+            <?php echo $total_price; ?>
+          </th>
+        </tr>
+        
+        
+    </table>
+  </body>
 </html>
+
